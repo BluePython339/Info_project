8e20aa45524141b5b791c3c98c79955978d5df73
bluepython339 <bluepython339@protonmail.com>
2021-12-20 10:18:43 +0100

    language profiler

--- wiki_scraper.py
+++ wiki_scraper.py
@@ -1,30 +1,69 @@
 import wikipedia as wiki
 import re
+from collections import Counter
+from tqdm import tqdm
 
 
 
-
-wiki.set_lang('nl')
-
-
-items = wiki.random(1000)
-
-a = wiki.page(items[0]).content
-print(re.sub(r'([^A-Za-z ])+', "", a))
-
-
-
-def preproccess_text(text):
+def preproccess_text(text, n):
     text = text.lower()
     stripped = re.sub(r'([^A-Za-z ])+', "", text)
     prepped = re.sub(r'([^A-Za-z])+', "_", stripped)
+    res = [prepped[i:i + n] for i in range(len(prepped) - n + 1)]
+    counts = list(zip(Counter(res).keys(),Counter(res).values()))
+    counts.sort(key=lambda tup: tup[1], reverse=True)
+    return counts
 
+if __name__ == "__main__":
 
+    wiki.set_lang('nl')
 
+    items = wiki.random(200)
 
+    #To create a suitable dataset we first want to know the most frequently appearing n-grams in the dutch language.
+    #
+    # code for tri-grams
+    trigrams = {}
+    for it in tqdm(items, "trigram making"):
+        try:
+            counts = preproccess_text(wiki.page(it).content, 3)
+            for i in counts:
+                if i[0] in trigrams.keys():
+                    trigrams[i[0]] += i[1]
+                else:
+                    trigrams[i[0]] = i[1]
+        except KeyboardInterrupt:
+            print('Interrupted')
+            exit(0)
+        except:
+            pass
 
+    tri_profile = [(k, v) for k, v in trigrams.items()]
+    tri_profile.sort(key=lambda tup: tup[1], reverse=True)
 
+    # code for quad-grams
+    quadgrams = {}
+    for it in tqdm(items, "quadgram making"):
+        try:
+            counts = preproccess_text(wiki.page(it).content, 4)
+            for i in counts:
+                if i[0] in quadgrams.keys():
+                    quadgrams[i[0]] += i[1]
+                else:
+                    quadgrams[i[0]] = i[1]
+        except KeyboardInterrupt:
+            print('Interrupted')
+            exit(0)
+        except :
+            pass
 
+    quad_profile = [(k, v) for k, v in quadgrams.items()]
+    quad_profile.sort(key=lambda tup: tup[1], reverse=True)
 
+    with open("tri_profile.bin", "w+") as tri:
+        for i in tri_profile:
+            tri.write(f"{i[0]} {i[1]}\n")
 
-wiki.set_lang('fr')
+    with open("quad_profile.bin", "w+") as quad:
+        for i in quad_profile:
+            quad.write(f"{i[0]} {i[1]}\n")
